2d80adb0da5deeae16f17f485ce08d0c16713f4d
hamdyamin28 <hamdyamin28123@gmail.com>
2024-07-12 02:02:50 +0300

    add sections

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleButton = document.getElementById('toggle-button');
-    const navLeft = document.getElementById('nav-left');
-    const navRight = document.getElementById('nav-right');
-
-    toggleButton.addEventListener('click', function() {
-        navLeft.classList.toggle('active');
-        navRight.classList.toggle('active');
-    });
+document.getElementById('toggle-button').addEventListener('click', function () {
+    document.getElementById('nav-items').classList.toggle('active');
 });
--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-
+//start header section//
 const heroText = document.querySelector('.hero-text');
 const heroImage = document.querySelector('.hero img');
 
 setTimeout(() => {
   heroText.classList.add('visible');
 }, 1500); // 1500 milliseconds = 1.5 seconds
+//end header section//
 
-// start footer section 
+
+
 
